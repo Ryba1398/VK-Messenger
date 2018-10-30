59fec4ea0c6ce2e5834acc06dc2c78351f5aebba
Nikolay Rybin <rubs1998@gmail.com>
2018-10-30 17:46:21 +0300

    Initial Commit

---
+++ VK Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Messenger
+//
+//  Created by Nikolay on 30/10/2018.
+//  Copyright © 2018 Nikolay Rybin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
